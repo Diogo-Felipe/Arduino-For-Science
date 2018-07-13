0108e30c54c1e4537c163c9cf9c37956e3fa17ee
Diogo L. C. Felipe <36720265+Diogo-Felipe@users.noreply.github.com>
2018-07-13 20:46:22 -0300

    Update funcoes_detector.ino.ino

--- Detectores de Voltagem/Voltagem x Tempo/DetectorDuploVoltagemTempo/funcoes_detector.ino.ino
+++ Detectores de Voltagem/Voltagem x Tempo/DetectorDuploVoltagemTempo/funcoes_detector.ino.ino
@@ -1,5 +1,5 @@
 //Função que realiza medida de voltagem instantânea
-float voltagemInstantanea(int porta){
+float medeVoltagem(int porta){
     
     float detectorValor = 0; //Variável para aquisição do valor
     float voltagemCorrigida = 0; //Valor corrigido e fornecido ao usuário (em milivolt)
@@ -9,26 +9,3 @@ float voltagemInstantanea(int porta){
 
     return voltagemCorrigida; //Retorno da voltagem corrigida
 }
-
-//Função que calcula a média no tempo para fornecer um valor médio da medida
-float medeVoltagem(int porta){
-  
-    int qtd = 10; //Quantidade de medidas instantâneas para se caulcular a média
-
-    int intervalo = 10; //Atraso atraso entre duas medidas instantâneas consecutivas(em milisegundo)
-    
-    float voltagemAcumulada = 0; //Variavél logica que acumula as voltagens instantâneas
-    
-    float media = 0; //Variável da média
-
-    //Acumulação das voltagens
-    for (int medida = 0; medida < qtd; medida++){
-         voltagemAcumulada = voltagemAcumulada + voltagemInstantanea(porta); 
-         delay(intervalo); //Implementação do intervalo
-    }
-    
-    media = voltagemAcumulada / qtd; //Calculo da média
-
-    return media; //Retono da média
-}
-
